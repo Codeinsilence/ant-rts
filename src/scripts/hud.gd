90e810132f13b42ac5de0c8799ddf36a9f0970fb
GP <hidden@mailfence.com>
2024-03-17 19:12:45 -0400

    files for ui + meat

---
+++ src/scripts/hud.gd
@@ -0,0 +1,14 @@
+# Initial Creator: George Power
+# Purpose: Adds dynamic behavior to the HUD
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#get_node("ActionPanel").visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
