c988fa6bae1452c88122081d7874bd5334dc08c6
GP <hidden@mailfence.com>
2024-03-17 19:36:02 -0400

    Action panel toggles when selecting units
    The action panel is hidden unless a unit is selected.

--- src/scripts/hud.gd
+++ src/scripts/hud.gd
@@ -2,13 +2,19 @@
 # Purpose: Adds dynamic behavior to the HUD
 extends Control
 
+@onready var action_panel = get_node("ActionPanel")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#get_node("ActionPanel").visible = false
+	action_panel.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(get_tree().get_nodes_in_group("selected_units").size() > 0 && action_panel.visible == false):
+		action_panel.visible = true
+	if(get_tree().get_nodes_in_group("selected_units").size() == 0 && action_panel.visible == true):
+		action_panel.visible = false
+	#for member in get_tree().get_nodes_in_group("selected_units"):
+				#print(member)
+				#if member.is_in_group("player"):
