7022e44ebd456192d9dd18ad5876e8df86ca2b78
GP <hidden@mailfence.com>
2024-03-30 20:24:57 -0400

    basic animation added
    ants will move when in transit, and will stop animating when stopped.

--- src/scripts/Worker.gd
+++ src/scripts/Worker.gd
@@ -5,6 +5,10 @@ var carry : Carrying
 var step = 0.01
 var t = 0.0
 var moving = false
+var animation_player: AnimationPlayer
+
+var last_location: Vector3
+var animation_threshold_dist = 0.01 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +17,31 @@ func _ready():
 	move = $Movement
 	carry = $Carrying
 	$SelectionRing.hide()
-	
 	super._ready()
+	
+	# Animation controls
+	last_location = location
+	animation_player = get_node("WorkerMeshAnimated/AnimationPlayer")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(moving == true):
+		animation_player.current_animation = "C4D Animation Take"
+	if(moving == false):
+		animation_player.current_animation = ""
+
+
+func _physics_process(delta):
+	location = self.global_position
+	
+	if location.distance_to(last_location) < animation_threshold_dist:
+		moving = false
+	else:
+		moving = true
+	
+	last_location = location
+	
+
 
 func set_destination(target:Vector3):
 	$Movement.set_destination(target)
