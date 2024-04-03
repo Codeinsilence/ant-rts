f905d5fb31f63348c95707a321a488bfbbde6494
GP <hidden@mailfence.com>
2024-04-03 19:22:05 -0400

    fixed texture bug

--- src/scripts/Worker.gd
+++ src/scripts/Worker.gd
@@ -11,6 +11,9 @@ var animation_player: AnimationPlayer
 var last_location: Vector3
 var animation_threshold_dist = 0.01 
 
+var ant_mesh: MeshInstance3D
+@onready var ant_texture = preload("res://assets/models/ant.jpg")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = 100.0;
@@ -24,6 +27,14 @@ func _ready():
 	# Animation controls
 	last_location = location
 	animation_player = get_node("WorkerMeshAnimated/AnimationPlayer")
+	
+	# stupid texture bug fix
+	ant_mesh = get_node("WorkerMeshAnimated/RootNode/\r\nant /skeleton /Skeleton3D/\r\nant _4")
+	var ant_mat := ant_mesh.mesh.surface_get_material(0)
+	ant_mat.albedo_texture = ant_texture
+	#print()
+	#ant_mesh.
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
